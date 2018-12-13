b5e112f4e29614e703ddc3c9a730cc54958fc9d7
Callum Terry <calham12@gmail.com>
2018-12-13 21:22:57 +0000

    Update MainKVN3.java

--- My leJOS Project/src/MainKVN3.java
+++ My leJOS Project/src/MainKVN3.java
@@ -7,7 +7,6 @@ public class MainKVN3 {
 		
 		Central central = new Central();
 		
-		//Stop stop = new Stop(central);
 		Red red = new Red(central);
 		Blue blue = new Blue(central);
 		Yellow yellow = new Yellow(central);
@@ -23,32 +22,3 @@ public class MainKVN3 {
 		sort.go();
 	}
 }
-
-/*CraneLower a = new CraneLower(crane, current, cs, mL, mR);
-CraneStop b = new CraneStop(crane, current, cs);
-ClawPick c = new ClawPick(claw, current);
-CraneRaise d = new CraneRaise(crane, current, claw); 
-
-Behavior[] behave1 = {b, a};
-Arbitrator sort = new Arbitrator(behave1);
-current.updateSortArbitrator(sort);
-
-Behavior[] behave2 = {c, d};
-Arbitrator pickup = new Arbitrator(behave2);
-current.updatePickupArbitrator(pickup); */
-
-
-
-/*new Thread(new Runnable() {
-     @Override
-     public void run() {
-          scan.displayColours();
-     }
-}).start(); */
-
-//sort.go();
-/*
-while (!(current.sorted)) {
-	sort.go();
-	pickup.go(); //I think this will only run AFTER the sort arbitrator is finished...
-}  */
