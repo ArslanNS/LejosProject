c190c7310a73f264a78aefd3b08aa47f5866e91a
CalhamZeKoala <calham12@gmail.com>
2018-12-14 07:36:28 +0000

    KVN3 fix

--- My leJOS Project/src/MainKVN3.java
+++ My leJOS Project/src/MainKVN3.java
@@ -5,20 +5,21 @@ public class MainKVN3 {
 
 	public static void main(String[] args) {
 		
-		Central central = new Central();
+		Central central = new Central(); //Creates an instance of the Central class
 		
-		Red red = new Red(central);
-		Blue blue = new Blue(central);
-		Yellow yellow = new Yellow(central);
-		Green green = new Green(central);
-		Behavior[] behave = {red, yellow, blue, green};
-		Arbitrator sort = new Arbitrator(behave);
+		Red red = new Red(central); //Creates an instance of the Red class
+		Blue blue = new Blue(central); //Creates an instance of the Blue class
+		Yellow yellow = new Yellow(central); //Creates an instance of the Yellow class
+		Green green = new Green(central);//Creates an instance of the Green class
+		Behavior[] behave = {red, yellow, blue, green}; //Creates a behaviour array containing all 4 behaviours
+		Arbitrator sort = new Arbitrator(behave); //Creates an instance of an arbitrator with the behaviour array as an argument
+
+		central.setSort(sort); //Sets the current instance of the arbitrator in the Central class (So that it can be stopped)
 		
-		Stop s = new Stop(central);
+		Stop s = new Stop(central); //Creates an instance of the Stop class
 		
 		s.start(); //Thread for stopping the arbitrator when the enter button is pressed
-		central.setSort(sort);
-		central.moveUntil();
-		sort.go();
+		central.moveUntil(); //Starts the conveyor belt
+		sort.go(); //Begins arbitration
 	}
 }
